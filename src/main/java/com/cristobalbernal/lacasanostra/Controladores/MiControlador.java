7b80f99aa3ce7a366096525dfb2a2385fdc53fcb
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2023-01-28 19:48:42 +0100

    Bebidas acabadas.

--- src/main/java/com/cristobalbernal/lacasanostra/Controladores/MiControlador.java
+++ src/main/java/com/cristobalbernal/lacasanostra/Controladores/MiControlador.java
@@ -13,13 +13,27 @@ public class MiControlador {
     public String carta(){
         return "Carta/bienvenidaCarta";
     }
-    @GetMapping("/entrantes")
+    @GetMapping("/bebidas")
     public String bebidas(){
+        return "Carta/Bebidas/Bebidas";
+    }
+    @GetMapping("/entrantes")
+    public String entrantes(){
         return "Carta/Entrantes/entrantes";
     }
     @GetMapping("/alcohol")
     public String alcohol(){
-        return "Carta/Bebidas_Alcoholicas/fafadfda";
+        return "Carta/Bebidas_Alcoholicas/bebidasAlcoholicas";
     }
+    @GetMapping("/ensaladas")
+    public String ensaladas(){return "Carta/Ensaladas/enladas";}
+    @GetMapping("/pastas")
+    public String pastas(){return "Carta/Ensaladas/enladas";}
+    @GetMapping("/hamburguesas")
+    public String hamburguesas(){return "Carta/Hambuguesas/Hamburguesas";}
+    @GetMapping("/pizzas")
+    public String pizzas(){return "Carta/Pizzas/Pizzas";}
+    @GetMapping("/postres")
+    public String postres(){return "Carta/Postres/Postres";}
 
 }
